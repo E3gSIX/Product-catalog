9ebe91b7cbb7a5c64a0fd4dc08c73856198f5645
TML45 <tassio.melo45@gmail.com>
2024-07-09 16:04:46 -0300

    feat: add base project structure

---
+++ src/main/java/com/e3gsix/fiap/tech_challenge_4_product_catalog/ProductCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.e3gsix.fiap.tech_challenge_4_product_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/e3gsix/fiap/tech_challenge_4_product_catalog/ProductCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e3gsix.fiap.tech_challenge_4_product_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
